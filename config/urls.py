81eecc7447b8d07082c40e161089f6c0dd83df6e
Aditya Giri Goswami <adityagiri@gkmit.co>
2026-01-07 08:03:41 +0530

    chore: include versioning in global api endpoint path

--- config/urls.py
+++ config/urls.py
@@ -21,8 +21,8 @@ from django.urls import path , include
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    path('api/', include('users.urls')),
-    path('api/', include('tasks.urls')),
+    path('api/v1/', include('users.urls')),
+    path('api/v1/', include('tasks.urls')),
 ]
 
 handler404 = "core.exceptions.custom_404_handler"
