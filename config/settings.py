77789acfcaaffe0437edddd2b48314e57517ffe1
Aditya Giri Goswami <adityagiri@gkmit.co>
2026-03-14 22:21:17 +0530

    fix: env variables

--- config/settings.py
+++ config/settings.py
@@ -205,7 +205,8 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER") 
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = "taskvault <no-reply@shopsurf.site>"
+
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
