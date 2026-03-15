86cd5c56579109c1598bf80f5b62ddbeee893938
Aditya Giri Goswami <adityagiri@gkmit.co>
2026-03-15 15:23:24 +0530

    chore: ci-cd check

--- config/settings.py
+++ config/settings.py
@@ -114,6 +114,10 @@ DATABASES = {
     }
 }
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+]
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
