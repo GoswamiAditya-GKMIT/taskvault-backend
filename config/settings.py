64a444577ccf6786cc8321467e4f3b5c3ea49068
Aditya Giri Goswami <adityagiri@gkmit.co>
2026-03-19 00:29:52 +0530

    chore: remove pyinstrument middleware

--- config/settings.py
+++ config/settings.py
@@ -80,7 +80,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'core.middleware.LoggingMiddleware',
-    'pyinstrument.middleware.ProfilerMiddleware',
+    # 'pyinstrument.middleware.ProfilerMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
